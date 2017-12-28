6b69cccdc740eb35d3af2d1e29b1b46360c9f8d1
qazsato <qazsato@gmail.com>
2017-12-29 00:47:06 +0900

    ベースレイアウト設定

--- src/components/AppTop.vue
+++ src/components/AppTop.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <div id="editor"></div>
+  <div id="screen">
+    <section id="toolbar">
+      <i class="material-icons">insert_link</i>
+      <i class="material-icons">insert_photo</i>
+      <i class="material-icons">format_bold</i>
+      <i class="material-icons">format_italic</i>
+      <i class="material-icons">format_quote</i>
+      <i class="material-icons">code</i>
+      <i class="material-icons">format_list_bulleted</i>
+      <i class="material-icons">format_list_numbered</i>
+      <i class="material-icons">publish</i>
+    </section>
+    <main>
+      <div id="editor"></div>
+      <div id="preview"></div>
+    </main>
   </div>
 </template>
 
@@ -15,9 +29,40 @@ export default {
 }
 </script>
 
+<style>
+  html {
+    height: 100%;
+  }
+  body {
+    height: 100%;
+    margin: 0;
+  }
+  #app {
+    height: 100%;
+  }
+</style>
+
 <style scoped>
-#editor {
-  width: 100%;
-  height: 500px;
-}
+  #screen {
+    height: 100%;
+  }
+
+  #toolbar {
+    height: 50px;
+  }
+
+  main {
+    display: flex;
+    height: calc(100% - 50px);
+  }
+
+  #editor,
+  #preview {
+    height: 100%;
+    width: 50%;
+  }
+
+  .material-icons {
+    margin: 10px;
+  }
 </style>
