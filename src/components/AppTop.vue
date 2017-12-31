357d2b9edbb06b831c82af1de720fb798631972f
qazsato <qazsato@gmail.com>
2018-01-01 02:56:27 +0900

    コードのシンタックスハイライト追加

--- src/components/AppTop.vue
+++ src/components/AppTop.vue
@@ -41,7 +41,14 @@
 
 <script>
 import 'github-markdown-css';
+import 'highlight.js/styles/default.css';
+
 import marked from 'marked';
+marked.setOptions({
+  highlight: (code) => {
+    return require('highlight.js').highlightAuto(code).value;
+  }
+});
 
 export default {
   data() {
