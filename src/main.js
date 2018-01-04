23d32ddf33991519a03728e305c2d67abbfbfac0
qazsato <qazsato@gmail.com>
2018-01-04 16:36:13 +0900

    ElementUI追加

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Element from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/ja';
+import 'element-ui/lib/theme-chalk/index.css';
 import AppTop from './components/AppTop.vue';
 import ace from 'ace-builds/src-min-noconflict/ace';
 import 'ace-builds/src-min-noconflict/theme-github';
@@ -7,7 +10,9 @@ import 'ace-builds/src-min-noconflict/mode-markdown';
 import './styles/main.css';
 import './fonts/icomoon/style.css';
 
+
 Vue.use(VueRouter);
+Vue.use(Element, {locale});
 
 const router = new VueRouter({
   routes: [
