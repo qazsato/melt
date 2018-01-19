ecec9209b1e978ffe44db80d4178107169f4a9b0
qazsato <qazsato@gmail.com>
2018-01-19 15:37:56 +0900

    ファイルツリー追加

--- src/components/AppTop.vue
+++ src/components/AppTop.vue
@@ -1,21 +1,28 @@
 <template>
   <div id="screen">
     <toolbar></toolbar>
-    <main>
-      <editor @changeText="changeText"></editor>
-      <preview :text="text"></preview>
-    </main>
+    <section>
+      <aside>
+        <filetree></filetree>
+      </aside>
+      <main>
+        <editor @changeText="changeText"></editor>
+        <preview :text="text"></preview>
+      </main>
+    </section>
   </div>
 </template>
 
 <script>
 import toolbar from './Toolbar.vue';
+import filetree from './Filetree.vue';
 import editor from './Editor.vue';
 import preview from './Preview.vue';
 
 export default {
   components: {
     toolbar,
+    filetree,
     editor,
     preview
   },
@@ -37,8 +44,18 @@ export default {
     height: 100%;
   }
 
+  section {
+    display: flex;
+    height: 100%;
+  }
+
+  aside {
+    width: 250px;
+  }
+
   main {
     display: flex;
+    flex: 1;
     height: calc(100% - 50px);
   }
 </style>
---
+++ src/components/Filetree.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="container">
+    <el-input class="filter-input" placeholder="Filter keyword" v-model="filterText" prefix-icon="el-icon-search" size="small"></el-input>
+    <el-tree class="file-tree" :data="data" @node-click="handleNodeClick" :filter-node-method="filterNode" ref="filetree">></el-tree>
+  </div>
+</template>
+
+<script>
+export default {
+  watch: {
+    filterText(val) {
+      this.$refs.filetree.filter(val);
+    }
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+    filterNode(value, data) {
+      if (!value) return true;
+      return data.label.indexOf(value) !== -1;
+    }
+  },
+  data() {
+    return {
+      filterText: '',
+      data: [{
+        label: 'Level one 1',
+        children: [{
+          label: 'Level two 1-1',
+          children: [{
+            label: 'Level three 1-1-1'
+          }]
+        }]
+      }, {
+        label: 'Level one 2',
+        children: [{
+          label: 'Level two 2-1',
+          children: [{
+            label: 'Level three 2-1-1'
+          }]
+        }, {
+          label: 'Level two 2-2',
+          children: [{
+            label: 'Level three 2-2-1'
+          }]
+        }]
+      }, {
+        label: 'Level one 3',
+        children: [{
+          label: 'Level two 3-1',
+          children: [{
+            label: 'Level three 3-1-1'
+          }]
+        }, {
+          label: 'Level two 3-2',
+          children: [{
+            label: 'Level three 3-2-1'
+          }]
+        }]
+      }],
+      defaultProps: {
+        children: 'children',
+        label: 'label'
+      }
+    };
+  }
+}
+</script>
+
+<style>
+  .el-tree-node:focus>.el-tree-node__content,
+  .el-tree-node__content:hover {
+    background-color: inherit;
+    opacity: 0.4;
+  }
+</style>
+
+<style scoped>
+  .container {
+    width: 100%;
+    height: 100%;
+    background-color: #4a4a4a;
+    border-top: 1px solid #24292e;
+  }
+
+  .filter-input {
+    margin: 10px;
+    width: calc(100% - 20px);
+  }
+
+  .file-tree {
+    color: #fff;
+    background-color: #4a4a4a;
+  }
+</style>
