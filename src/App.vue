7dd0cd525adf73f992dbf2cd6c7247133b55e8a3
qazsato <qazsato@gmail.com>
2018-01-24 01:25:59 +0900

    起動時のエントリー用コンポーネント作成

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import Element from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/ja';
+import 'element-ui/lib/theme-chalk/index.css';
+import './assets/styles/main.css';
+import './assets/fonts/icomoon/style.css';
+Vue.use(Element, {locale});
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import Vue from 'vue';
 import router from './router/index';
 import store from './store/index';
-import Element from 'element-ui';
-import locale from 'element-ui/lib/locale/lang/ja';
-import 'element-ui/lib/theme-chalk/index.css';
-import './assets/styles/main.css';
-import './assets/fonts/icomoon/style.css';
+import App from './App.vue';
 
-Vue.use(Element, {locale});
-
-new Vue({router, store}).$mount('#app');
+new Vue({
+  el: '#app',
+  router,
+  store,
+  render: h => h(App),
+});
