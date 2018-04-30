f895d4270e21b87e667818f486bfa96eba55cd4f
qazsato <qazsato@gmail.com>
2018-05-01 04:54:51 +0900

    プレビュー処理のロジックを分割

---
+++ src/assets/scripts/MarkdownPreview.js
@@ -0,0 +1,46 @@
+const MarkdownIt = require('markdown-it');
+const emoji = require('markdown-it-emoji');
+const checkbox = require('markdown-it-task-checkbox');
+const highlight = require('highlight.js');
+
+class MarkdownPreview {
+  constructor() {
+    this.md = new MarkdownIt({
+      html: true,
+      highlight: (code, lang) => {
+        if (lang) {
+          return highlight.highlightAuto(code, [lang]).value;
+        }
+        return code;
+      }
+    });
+    this.md.use(emoji);
+    this.md.use(checkbox, {disabled: true});
+
+    // NOTE 全てのaタグにtarget="_blank"を付与する
+    // https://github.com/markdown-it/markdown-it/blob/0e51825a5cd912121d733938ef2603833378888a/docs/architecture.md#renderer
+    // Remember old renderer, if overriden, or proxy to default renderer
+    const defaultRender = this.md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
+      return self.renderToken(tokens, idx, options);
+    };
+
+    this.md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
+      // If you are sure other plugins can't add `target` - drop check below
+      const aIndex = tokens[idx].attrIndex('target');
+
+      if (aIndex < 0) {
+        tokens[idx].attrPush(['target', '_blank']); // add new attribute
+      } else {
+        tokens[idx].attrs[aIndex][1] = '_blank';    // replace value of existing attr
+      }
+
+      // pass token to default renderer.
+      return defaultRender(tokens, idx, options, env, self);
+    };
+  }
+  render(text) {
+    return this.md.render(text);
+  }
+}
+
+module.exports = MarkdownPreview;
--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -6,13 +6,9 @@
 </template>
 
 <script>
-import MarkdownIt from 'markdown-it';
-import emoji from 'markdown-it-emoji';
-import checkbox from 'markdown-it-task-checkbox';
-import highlight from 'highlight.js';
-import 'highlight.js/styles/github.css';
-import 'github-markdown-css';
+import Markdown from '../assets/scripts/MarkdownPreview';
 import publishButton from './PublishButton.vue';
+import '../assets/styles/markdown-preview.scss';
 
 export default {
   props: ['text'],
@@ -20,54 +16,15 @@ export default {
     publishButton
   },
   created() {
-    this.md = new MarkdownIt({
-      html: true,
-      highlight: (code, lang) => {
-        if (lang) {
-          return highlight.highlightAuto(code, [lang]).value;
-        }
-        return code;
-      }
-    });
-    this.md.use(emoji);
-    this.md.use(checkbox, {disabled: true});
-
-    // NOTE 全てのaタグにtarget="_blank"を付与する
-    // https://github.com/markdown-it/markdown-it/blob/0e51825a5cd912121d733938ef2603833378888a/docs/architecture.md#renderer
-    // Remember old renderer, if overriden, or proxy to default renderer
-    const defaultRender = this.md.renderer.rules.link_open || function(tokens, idx, options, env, self) {
-      return self.renderToken(tokens, idx, options);
-    };
-
-    this.md.renderer.rules.link_open = function (tokens, idx, options, env, self) {
-      // If you are sure other plugins can't add `target` - drop check below
-      const aIndex = tokens[idx].attrIndex('target');
-
-      if (aIndex < 0) {
-        tokens[idx].attrPush(['target', '_blank']); // add new attribute
-      } else {
-        tokens[idx].attrs[aIndex][1] = '_blank';    // replace value of existing attr
-      }
-
-      // pass token to default renderer.
-      return defaultRender(tokens, idx, options, env, self);
-    };
-
-
+    this.markdown = new Markdown();
   },
   computed: {
     markedText() {
-      return this.md.render(this.text);
+      return this.markdown.render(this.text);
     }
   }
 }
 </script>
-
-<style>
-  .markdown-body .task-list-item input {
-    margin: 0 0.2em 0.25em -1.4em;
-  }
-</style>
 
 <style scoped>
   .preview-area {
