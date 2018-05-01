9a762aeebff137134a406cc7afefe28375c8c841
qazsato <qazsato@gmail.com>
2018-05-02 00:01:10 +0900

    起動時の画面サイズを最大に変更

--- main.js
+++ main.js
@@ -14,7 +14,9 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow()
+
+  mainWindow.maximize()
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
