87fb30fe6d50f575571019150b13499c0b6207ff
qazsato <qazsato@gmail.com>
2018-05-16 15:13:14 +0900

    .vueのテスト実行環境整備

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  moduleFileExtensions: ['js', 'json', 'vue'],
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
+  },
+  snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/assets/scripts/**/*.js']
+  collectCoverageFrom: [
+    'src/assets/scripts/**/*.js',
+    'src/**/*.vue',
+  ]
 };
