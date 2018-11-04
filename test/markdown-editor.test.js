6dc1e8393e618b54a1a747c82b3daf46ed991bde
qazsato <qazsato@gmail.com>
2018-11-04 22:52:03 +0900

    Add test case

--- test/markdown-editor.test.js
+++ test/markdown-editor.test.js
@@ -5,3 +5,129 @@ test('エディタを生成する', () => {
   const editor = new Editor('editor');
   expect(editor).toBeDefined();
 });
+
+test('リンクを挿入する', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  editor.insertLink('title', 'https://sample.com');
+  expect(editor.getText()).toBe('[title](https://sample.com)');
+});
+
+test('画像を挿入する', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  editor.insertImage('alt', 'https://sample.com/test.png');
+  expect(editor.getText()).toBe('![alt](https://sample.com/test.png)');
+});
+
+test('太字を挿入する', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  editor.insertBold();
+  expect(editor.getText()).toBe('****');
+});
+
+test('斜体を挿入する', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  editor.insertItalic();
+  expect(editor.getText()).toBe('**');
+});
+
+test('打ち消し線を挿入する', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  editor.insertStrikethrough();
+  expect(editor.getText()).toBe('~~~~');
+});
+
+test('コードを挿入する', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  editor.insertCode();
+  expect(editor.getText()).toBe('``');
+});
+
+test('引用を挿入する', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  editor.insertQuote();
+  expect(editor.getText()).toBe('> ');
+});
+
+test('箇条書きリストを挿入する', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  editor.insertBulletedList();
+  expect(editor.getText()).toBe('- ');
+});
+
+test('番号付きリストを挿入する', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  editor.insertNumberedList();
+  expect(editor.getText()).toBe('1. ');
+});
+
+test('チェックリストを挿入する', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  editor.insertCheckedList();
+  expect(editor.getText()).toBe('- [ ] ');
+});
+
+test('箇条書きリストか判定する(true)', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  expect(editor.isBulletedList('- test')).toBeTruthy();
+});
+
+test('箇条書きリストか判定する(false)', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  expect(editor.isBulletedList('test')).toBeFalsy();
+});
+
+test('番号付きリストか判定する(true)', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  expect(editor.isNumberedList('1. test')).toBeTruthy();
+});
+
+test('番号付きリストか判定する(false)', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  expect(editor.isNumberedList('test')).toBeFalsy();
+});
+
+test('チェックリストか判定する チェック未(true)', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  expect(editor.isCheckedList('- [ ] test')).toBeTruthy();
+});
+
+test('チェックリストか判定する チェック済(true)', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  expect(editor.isCheckedList('- [x] test')).toBeTruthy();
+});
+
+test('チェックリストか判定する(false)', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  expect(editor.isCheckedList('test')).toBeFalsy();
+});
+
+test('テーブルを挿入する', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  editor.insertTable();
+  const table = `  |  | \n:-|:-|:-\n  |  | \n  |  | \n`;
+  expect(editor.getText()).toBe(table);
+});
+
+test('タイトルを取得する', () => {
+  document.body.innerHTML = '<textarea id="editor"></textarea>';
+  const editor = new Editor('editor');
+  expect(editor.getTitle()).toBe('');
+});
