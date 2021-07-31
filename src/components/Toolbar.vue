e546a7140bdc8e3def58d3ff5ccb417e2ed65a07
qazsato <qazsato@gmail.com>
2021-07-31 23:32:28 +0900

    タイトル追加

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -1,5 +1,14 @@
 <template>
   <section id="toolbar">
+    <h1 class="file-name">
+      <el-tooltip
+        :content="$store.state.currentFile"
+        :open-delay="1000"
+      >
+        <div>{{ fileName }}</div>
+      </el-tooltip>
+    </h1>
+
     <el-radio-group
       v-model="mode"
       class="checkbox-mode"
@@ -17,11 +26,20 @@
 </template>
 
 <script>
+import Note from '@scripts/note/note.js';
+
 export default {
   data() {
     return {
       mode: this.$store.state.mode
     };
+  },
+  computed: {
+    fileName() {
+      if (!this.$store.state.currentFile) return;
+      const title = new Note(this.$store.state.currentFile).readTitle();
+      return title || 'Untitled';
+    }
   },
   methods: {
     changeViewMode() {
@@ -35,6 +53,22 @@ export default {
   #toolbar {
     height: 50px;
     background-color: #4a4a4a;
+  }
+
+  .file-name {
+    display: flex;
+    align-items: center;
+    margin: 0 10px;
+    height: 50px;
+    color: #fff;
+  }
+
+  .file-name div {
+    font-size: 18px;
+    font-weight: bold;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
   }
 
   .checkbox-mode {
