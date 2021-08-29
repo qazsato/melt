d2156e193746e30886890c032a190a0c104e43de
qazsato <qazsato@gmail.com>
2021-08-29 09:48:02 +0900

    ts-jest 設定

--- jest.config.js
+++ jest.config.js
@@ -1,21 +1,22 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFiles: ['./test/jsdom-polyfill.js'],
-  moduleFileExtensions: ['js', 'json', 'vue'],
+  moduleFileExtensions: ['ts', 'js', 'json', 'vue'],
   transform: {
+    '^.+\\.ts$': '<rootDir>/node_modules/ts-jest',
     '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
     '.*\\.(vue)$': '<rootDir>/node_modules/vue-jest'
   },
   snapshotSerializers: ['<rootDir>/node_modules/jest-serializer-vue'],
   collectCoverage: true,
   collectCoverageFrom: [
-    'src/assets/scripts/**/*.js',
-    'src/utils/**/*.js',
+    'src/assets/scripts/**/*.ts',
+    'src/utils/**/*.ts',
     'src/**/*.vue'
   ],
   testPathIgnorePatterns: ['/node_modules/', '/release-builds/'],
   moduleNameMapper: {
-    '^@(.*)$': '<rootDir>/src$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
     '^@config(.*)$': '<rootDir>/config$1'
   }
 }
