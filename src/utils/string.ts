d8878a0613b94e9449095214b3fd834a0d7504ef
qazsato <qazsato@gmail.com>
2022-02-09 08:00:36 +0900

    全角半角判定の高速化

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -6,10 +6,12 @@ export const getCharWidth = (str: string): number => {
   let sbc = 0 // シングルバイト文字
   let mbc = 0 // マルチバイト文字
   for (let i = 0; i < str.length; i++) {
-    if (new Blob([str[i]]).size > 1) {
-      mbc++
-    } else {
+    // NOTE: 半角スペースからチルダまでが、半角英数字と記号が定義された範囲となるため、その範囲内の場合シングルバイト文字と判断。
+    // https://www-creators.com/archives/5187
+    if (str[i].match(/[ -~]/)) {
       sbc++
+    } else {
+      mbc++
     }
   }
   // NOTE: Source Han Code JP は、半角3文字に対して全角2文字のため、全角を1.5倍している
