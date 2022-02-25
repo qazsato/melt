4f33965ded80494080a7448a84aa8bf2c68a55a8
qazsato <qazsato@gmail.com>
2022-02-25 09:12:41 +0900

    mermaid の export 構文による Jest 実行エラー対応

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,5 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript',
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts', 'src/**/*.vue'],
+  transformIgnorePatterns: [],
 }
