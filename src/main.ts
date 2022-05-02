ba4f12057552058d67592ca50699b4df74db4f52
qazsato <qazsato@gmail.com>
2022-05-02 09:57:38 +0900

    element plus 設定

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import { store } from './store'
 import App from './app.vue'
 import * as Sentry from '@sentry/vue'
 import { BrowserTracing } from '@sentry/tracing'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
 app.use(router)
 
 app.use(store)
+
+app.use(ElementPlus)
 
 app.mount('#app')
 
