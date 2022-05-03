e4512783d80245a9434dcb5b77239512500339cd
qazsato <qazsato@gmail.com>
2022-05-03 11:01:20 +0900

    webpack v4 に対応しているバージョンまで更新

--- vue.config.js
+++ vue.config.js
@@ -18,7 +18,7 @@ module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        prependData: `@use "./src/assets/styles/_variables.scss" as var;`,
+        additionalData: `@use "./src/assets/styles/_variables.scss" as var;`,
       },
     },
   },
